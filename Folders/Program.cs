5a96e0fee520c664f4371df20ecb299a8d447c57
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-16 00:22:12 +0100

    My Solution works 100% but is not the best

---
+++ Folders/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Folders
+{
+    public static IEnumerable<string> FolderNames(string xml, char startingLetter)
+    {
+        
+
+        List<string> response = new List<string>();
+
+        
+
+        while (xml.IndexOf("<folder name") != -1)
+        {
+            var index = xml.IndexOf("<folder name");
+            var removeEverythingBeforeFolderName = xml.Substring(index);
+            xml = removeEverythingBeforeFolderName;
+            
+            index = xml.IndexOf("\"") + 1;
+            var removeBeforeQuote = xml.Substring(index);
+            xml = removeBeforeQuote;
+
+            index = xml.IndexOf("\"");
+            var folderName = xml.Substring(0, index);
+            if (folderName[0] == startingLetter)
+                response.Add(folderName);
+        }
+ 
+        return response;
+    }
+
+    public static void Main(string[] args)
+    {
+        string xml =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+            "<folder name=\"c\">" +
+                "<folder name=\"program files\">" +
+                    "<folder name=\"uninstall information\" />" +
+                "</folder>" +
+                "<folder name=\"users\" />" +
+            "</folder>";
+
+        foreach (string name in Folders.FolderNames(xml, 'u'))
+            Console.WriteLine(name);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Folders
-Implement a function FolderNames, which accepts a string containing an XML file that specifies folder structure and returns all folder names that start with startingLetter. 
+Implement a function FolderNames, which accepts a string containing an XML file that specifies folder structure and returns all folder names that start with startingLetter.The XML format is given in the example below.
+
+For example, for the letter 'u' and XML file:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<folder name="c">
+    <folder name="program files">
+        <folder name="uninstall information" />
+    </folder>
+    <folder name="users" />
+</folder> 
+
+
